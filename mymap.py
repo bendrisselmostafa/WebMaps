e8df05368da9ab8549e45324ee4b6e263af5c7d2
BENDRISS <elm.bendriss@gmail.com>
2021-07-17 13:46:51 +0100

    Add Marker to exact location

--- mymap.py
+++ mymap.py
@@ -1,5 +1,17 @@
 import folium
 
-map = folium.Map(location=[33.60881457164729, -7.632757310367674], zoom_start=8)
+map = folium.Map(
+    location=[33.57, -7.59],
+    zoom_start=12,
+    tiles="Stamen Terrain",
+)
+
+map.add_child(
+    folium.Marker(
+        location=[33.60881457164729, -7.632757310367674],
+        popup="Here Is Hassan II Mosque",
+        icon=folium.Icon(color="green"),
+    )
+)
 
 map.save("Map.html")
